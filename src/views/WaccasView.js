7dfddb44eb49f5cb879b2d334c75f4463ff9ba25
William Johnston <wjoh0002@student.monash.edu>
2019-11-30 17:34:13 +1100

    added WACCAZZ

--- src/views/WaccasView.js
+++ src/views/WaccasView.js
@@ -3,9 +3,12 @@
 /* eslint-disable react/prop-types */
 
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { tw } from "react-native-tailwindcss";
 import { AppContext } from "../../AppContext";
+
+import burgerImage from "../../assets/burger.png";
+import maccasLogoImage from "../../assets/maccas.png";
 
 class WaccasViewComponent extends Component {
   static navigationOptions = ({ navigation }) => {
@@ -13,6 +16,21 @@ class WaccasViewComponent extends Component {
       title: navigation.getParam(`headerTitle`, `Pockets`)
     };
   };
+
+  styles = StyleSheet.create({
+    rotateImage: {
+      transform: [{ rotate: `180deg` }]
+    },
+    buyNowButton: {
+      marginRight: 40,
+      marginLeft: 40,
+      marginTop: 10,
+      paddingTop: 10,
+      paddingBottom: 10,
+      backgroundColor: `#ffc300`,
+      borderRadius: 2
+    }
+  });
 
   constructor() {
     super();
@@ -29,17 +47,69 @@ class WaccasViewComponent extends Component {
     const { expanded } = this.state;
 
     return (
-      <View style={[tw.wFull, tw.hFull, tw.relative, tw.flex, tw.itemsStretch]}>
-        <Text>Gday M8</Text>
-        <View style={[tw.bgRed100]}>
-          <Text>This is waccas!!!</Text>
+      <View
+        style={
+          ([tw.wFull, tw.hFull, tw.relative, tw.itemsStretch, tw.p4],
+          {
+            backgroundColor: `#dd1021`
+          })
+        }
+      >
+        <View
+          style={[
+            tw.wFull,
+            tw.hFull,
+            tw.itemsCenter,
+            tw.flex,
+            tw.flexCol,
+            tw.justifyBetween,
+            tw.pT8,
+            tw.pB16
+          ]}
+        >
+          <Image
+            style={[
+              { width: 270, height: 250, backgroundColor: `transparent` },
+              this.styles.rotateImage
+            ]}
+            source={maccasLogoImage}
+          />
+          <Image
+            style={{ width: 282, height: 200, backgroundColor: `transparent` }}
+            source={burgerImage}
+          />
+          <View
+            style={[
+              tw.bgWhite,
+              tw.p4,
+              tw.mT8,
+              tw.rounded,
+              tw.flex,
+              tw.flexCol,
+              tw.justifyBetween,
+              tw.m8,
+              {
+                width: 360
+              }
+            ]}
+          >
+            <Text style={[tw.fontBold, tw.text4xl, tw.pY2]}>
+              Today&apos;s deal
+            </Text>
+            <Text style={[tw.fontSemibold, tw.pY2]}>$1 shitty hamburgers!</Text>
+            <TouchableOpacity
+              style={[this.styles.buyNowButton, tw.itemsCenter, tw.pY2]}
+            >
+              <Text>Buy now!</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
   }
 }
 
-const PocketView = props => {
+const WaccasView = props => {
   const { navigation } = props;
 
   return (
@@ -56,4 +126,4 @@ const PocketView = props => {
   );
 };
 
-export default PocketView;
+export default WaccasView;
